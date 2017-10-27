0e8d096bc93d1447b33bfa188574967f3176bc8c
daocong <594277625@qq.com>
2017-10-27 11:53:50 +0800

    Add files via upload

---
+++ Module.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace wc\model;
+
+/**
+ * Description of Module
+ *
+ * @author wc
+ */
+class Module extends \yii\base\Module{
+    //put your code here
+    public function init() {
+        parent::init();
+        \Yii::configure($this, require_once __DIR__.'config.php');
+    }
+}
